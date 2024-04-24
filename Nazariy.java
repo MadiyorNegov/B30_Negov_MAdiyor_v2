660d3c8f3629c0d943485b274d78b87f8164bb8a
MadiyorNegov <madiyor033@gmail.com>
2024-04-24 09:34:26 +0500

    s

---
+++ Nazariy.java
@@ -0,0 +1,19 @@
+public class Nazariy {
+
+    // 1. Arraylist malumotlarni arrayda Linklist esa Node da saqlaydi.
+
+    // 2. LinkedHashMap da elementlar qo'shish tartibida saqlaydi
+
+
+    //3.Generic bizga qaysidir class yoki metodni turli xil ob'ektlar uchun ishlatish.
+    // Ya'ni biz yozgan class yoki metod umumiy bo'lishligi.
+
+  // 4.  Collection heirarcy ->  Iterabl interfacedan extend olgan ->
+    // Collection  -> Set, queue, lIst ->
+    // Setdan HashSet, LinkedHashSet ,
+    // Lisetdan -> ArrayLIst,LinkList,Vektor
+    // Queuedan -> ArrayDequeue, PrioriryQueue extend olgan.
+
+
+
+}
